33990d26c5b9f331b19f16062f17b752607e512e
Deneuville Fabian <deneuville.fabian@live.fr>
2022-09-01 21:25:28 +0200

    P2C1 exercise done

--- src/components/Loader/loader.jsx
+++ src/components/Loader/loader.jsx
@@ -10,13 +10,14 @@ const rotate = keyframes`
 `
 
 const Loader = styled.div`
+    margin: 10px auto;
     paddin: 10px;
     border: 6px solid ${colors.primary};
     border-bottom-color: transparent;
     border-radius: 22px;
     animation: ${rotate} 1s infinite linear;
-    height: 0;
-    width: 0;
+    height: 25px;
+    width: 25px;
 `
 
 export default Loader;
--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,20 +1,22 @@
 import Card from '../../components/Card';
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import Loader from '../../components/Loader/loader';
 
-const freelanceProfiles = [
-    {
-        name: 'Jane Doe',
-        jobTitle: 'Devops',
-    },
-    {
-        name: 'John Doe',
-        jobTitle: 'Developpeur frontend',
-    },
-    {
-        name: 'Jeanne Biche',
-        jobTitle: 'Developpeuse Fullstack',
-    }
-]
+// const freelanceProfiles = [
+//     {
+//         name: 'Jane Doe',
+//         jobTitle: 'Devops',
+//     },
+//     {
+//         name: 'John Doe',
+//         jobTitle: 'Developpeur frontend',
+//     },
+//     {
+//         name: 'Jeanne Biche',
+//         jobTitle: 'Developpeuse Fullstack',
+//     }
+// ]
 
 const CardsContainer = styled.div`
     display: grid;
@@ -37,24 +39,77 @@ const PageSubTitle = styled.h2`
     padding-bottom: 30px;
 `
 
+const ErrorMessage = styled.p`
+    padding: 20px;
+    color: crimson;
+    text-align: center
+`
+
 function Freelances(){
+
+    const [freelanceProfiles, setFreelanceProfiles] = useState([]);
+    const [isDataLoading, setDataLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    // useEffect(() => {
+    //     setDataLoading(true);
+    //     fetch('http://localhost:8000/freelances')
+    //     .then((response) => response.json()
+    //     .then(({freelancersList}) => {
+    //         setFreelanceProfiles(freelancersList)
+    //         setDataLoading(false);
+    //     }))
+    //     .catch((error) => console.log(error))
+    // }, [])
+
+    useEffect(() => {
+        async function fetchProfiles(){
+            setDataLoading(true)
+            try {
+                const response = await fetch('http://localhost:8000/freelances');
+                const { freelancersList } = await response.json();
+                setFreelanceProfiles(freelancersList);
+            }
+            catch(error) {
+                console.log(error);
+                setError(true);
+            }
+            finally {
+                setDataLoading(false);
+            }
+        }
+        fetchProfiles()
+    }, [])
+
+    if(error){
+        return (
+            <ErrorMessage>Oups, il semble y avoir un problème.</ErrorMessage>
+        )
+    }
+
     return (
         <div>
             <PageTitle>Trouvez votre prestataire</PageTitle>
 
             <PageSubTitle>Chez Shiny nous réunissons les meilleurs profils pour vous.</PageSubTitle>
 
-            <CardsContainer>
-                {freelanceProfiles.map((profile, index) => {
-                    return (
-                        <Card
-                            key={`${profile.name} - ${index}`}
-                            label={profile.jobTitle}
-                            title={profile.name}
-                        />
-                    )
-                })}
-            </CardsContainer>
+            {isDataLoading ? (
+                <Loader/>
+            ) : (
+                <CardsContainer>
+                    {freelanceProfiles.map((profile, index) => {
+                        return (
+                            <Card
+                                key={profile.id}
+                                label={profile.job}
+                                title={profile.name}
+                                picture={profile.picture}
+                            />
+                        )
+                    })}
+                </CardsContainer>
+            )}
+
         </div>
     )
 }
