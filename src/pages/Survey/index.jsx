3cda2d679de2e6db9ad42b789613ff0bf571d47a
Deneuville Fabian <deneuville.fabian@live.fr>
2022-09-02 12:32:44 +0200

    Creating custom useFetch hook to handle survey API request

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import colors from '../../utils/style/color';
 import Loader from '../../components/Loader/loader';
 import { SurveyContext } from '../../utils/context';
 import { useContext } from 'react';
+import { useFetch } from '../../utils/hooks';
 
 
 const SurveyContainer = styled.div`
@@ -70,10 +71,10 @@ function Survey(){
     const questionNumberInt = parseInt(questionNumber); 
     const previousQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
     const nextQuestionNumber = questionNumberInt + 1;
-    const [surveyData, setSurveyData] = useState({});
-    const [isDataLoading, setDataLoading] = useState(false);
+    // const [surveyData, setSurveyData] = useState({});
+    // const [isDataLoading, setDataLoading] = useState(false);
     const {answers, saveAnswers} = useContext(SurveyContext)
-    const [error, setError] = useState(false);
+    // const [error, setError] = useState(false);
 
     // useEffect(() => {
     //     setDataLoading(true)
@@ -90,24 +91,28 @@ function Survey(){
         saveAnswers({[questionNumber] : answer})
     }
 
-    useEffect(() => {
-        async function fetchSurvey(){
-            setDataLoading(true)
-            try {
-                const response = await fetch(`http://localhost:8000/survey`)
-                const { surveyData } = await response.json()
-                setSurveyData(surveyData)
-            }
-            catch(error) {
-                console.log(error);
-                setError(true)
-            }
-            finally {
-                setDataLoading(false)
-            }
-        }
-        fetchSurvey()
-    }, [])
+    // useEffect(() => {
+    //     async function fetchSurvey(){
+    //         setDataLoading(true)
+    //         try {
+    //             const response = await fetch(`http://localhost:8000/survey`)
+    //             const { surveyData } = await response.json()
+    //             setSurveyData(surveyData)
+    //         }
+    //         catch(error) {
+    //             console.log(error);
+    //             setError(true)
+    //         }
+    //         finally {
+    //             setDataLoading(false)
+    //         }
+    //     }
+    //     fetchSurvey()
+    // }, [])
+
+    const { data, isLoading, error } = useFetch(`http://localhost:8000/survey`);
+
+    const { surveyData } = data;
 
     if(error){
         return (
@@ -119,10 +124,10 @@ function Survey(){
         <SurveyContainer>
             <h1>Questionnaire</h1>
             <QuestionTitle>Question numéro {questionNumber}</QuestionTitle>
-            {isDataLoading ? (
+            {isLoading ? (
                 <Loader/>
             ) : (
-                <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+                <QuestionContent>{ surveyData && surveyData[questionNumber]}</QuestionContent>
             )}
 
             <ReplyWrapper>
